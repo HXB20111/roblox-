b5d30c02f0f94447126d96b1e02b6bbe6936a1a5
HXB20111 <a2011916@qq.com>
2025-07-28 11:08:03 +0800

    菁脚本.lua

--- 菁脚本.lua
+++ 菁脚本.lua
@@ -1,37 +1,121 @@
-    local CoreGui = game:GetService("StarterGui")
+local OrionLib =loadstring(game:HttpGet("https://pastebin.com/raw/FUEx0f3G"))()
+local LBLG = Instance.new("ScreenGui", getParent)
+local LBL = Instance.new("TextLabel", getParent)
+local player = game.Players.LocalPlayer
 
-CoreGui:SetCore("SendNotification", {
-    Title = "你的脚本名称（菁脚本）",
-    Text = "正在加载",
-    Duration = 5, 
+LBLG.Name = "LBLG"
+LBLG.Parent = game.CoreGui
+LBLG.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+LBLG.Enabled = true
+LBL.Name = "LBL"
+LBL.Parent = LBLG
+LBL.BackgroundColor3 = Color3.new(1, 1, 1)
+LBL.BackgroundTransparency = 1
+LBL.BorderColor3 = Color3.new(0, 0, 0)
+LBL.Position = UDim2.new(0.75,0,0.010,0)
+LBL.Size = UDim2.new(0, 133, 0, 30)
+LBL.Font = Enum.Font.GothamSemibold
+LBL.Text = "TextLabel"
+LBL.TextColor3 = Color3.new(1, 1, 1)
+LBL.TextScaled = true
+LBL.TextSize = 14
+LBL.TextWrapped = true
+LBL.Visible = true
+
+local FpsLabel = LBL
+local Heartbeat = game:GetService("RunService").Heartbeat
+local LastIteration, Start
+local FrameUpdateTable = { }
+
+local function HeartbeatUpdate()
+	LastIteration = tick()
+	for Index = #FrameUpdateTable, 1, -1 do
+		FrameUpdateTable[Index + 1] = (FrameUpdateTable[Index] >= LastIteration - 1) and FrameUpdateTable[Index] or nil
+	end
+	FrameUpdateTable[1] = LastIteration
+	local CurrentFPS = (tick() - Start >= 1 and #FrameUpdateTable) or (#FrameUpdateTable / (tick() - Start))
+	CurrentFPS = CurrentFPS - CurrentFPS % 1
+	FpsLabel.Text = ("北京时间:"..os.date("%H").."时"..os.date("%M").."分"..os.date("%S"))
+end
+Start = tick()
+Heartbeat:Connect(HeartbeatUpdate)
+local Window = OrionLib:MakeWindow({Name = "菁脚本", HidePremium = false, SaveConfig = true,IntroText = "菁脚本", ConfigFolder = "菁脚本"})
+
+game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "菁脚本"; Text ="欢迎使用菁脚本"; Duration = 4; })
+
+local about = Window:MakeTab({
+    Name = "作者信息",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+
 })
 
-print("反挂机开启")
-		local vu = game:GetService("VirtualUser")
-		game:GetService("Players").LocalPlayer.Idled:connect(function()
-		   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-		   wait(1)
-		   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-		end)
-		
-local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/HXB20111/roblox-/refs/heads/main/%E9%BB%84%E8%84%9A%E6%9C%ACUI.lua'))()
-local Window = OrionLib:MakeWindow({Name = "菁脚本", HidePremium = false, SaveConfig = true,IntroText = "欢迎使用菁脚本", ConfigFolder = "欢迎菁脚本"})
+about:AddParagraph("作者: Hfh916")
+about:AddParagraph("作者qq: 1357377308")
+about:AddParagraph("qq群: 没有")
 
 local about = Window:MakeTab({
     Name = "玩家信息",
-    Icon = "rbxassetid://11109742737",
+    Icon = "rbxassetid://4483345998",
     PremiumOnly = false
+
 })
 
 about:AddParagraph("您的用户名:"," "..game.Players.LocalPlayer.Name.."")
 about:AddParagraph("您的注入器:"," "..identifyexecutor().."")
-about:AddParagraph("QQ群 : ","未知")
+about:AddParagraph("您当前服务器的ID"," "..game.GameId.."")
+
+local Tab =Window:MakeTab({
+
+	Name = "复制作者信息",
+
+	Icon = "rbxassetid://4483345998",
+
+	PremiumOnly = false
+
+})
+
+Tab:AddButton({
+
+	Name = "复制作者QQ",
+
+	Callback = function()
+
+     setclipboard("1357377308")
+
+  	end
+
+})
+
+Tab:AddButton({
+
+	Name = "复制QQ群",
+
+	Callback = function()
+
+     setclipboard("没有")
+
+  	end
+
+})
+
+OrionLib:MakeNotification({
+	Name = "菁脚本",
+	Content = "欢迎使用菁脚本",
+	Image = "rbxassetid://4483345998",
+	Time = 2
+
+})
 
 local Tab = Window:MakeTab({
-  Name = "玩家功能",
-  Icon = "rbxassetid://4483345998",
-  PremiumOnly = false
-  })
+
+    Name = "通用",
+
+    Icon = "rbxassetid://4483345998",
+
+    PremiumOnly = false
+
+})
 
 -- 文本框配置函数
 local function addSettingTextbox(tab, config)
